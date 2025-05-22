2f286650905bc9df01706dcfb3930bfbdca2bd45
abirami229 <abiramisundarim23aid@vetias.ac.in>
2025-05-22 09:50:16 +0530

    Add files via upload

---
+++ day 7/file2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;//{1,3,6}
+    scanf("%d",&n);// n==3 
+    int arr[n];//{1,3,6,0}
+    for (int i=0; i<n;i++){
+        scanf("%d",&arr[i]);
+     }
+     int pos;
+     scanf("%d",&pos);
+     for(int 1=pos;i)
+}
